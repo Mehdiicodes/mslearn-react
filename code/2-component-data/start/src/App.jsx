1452de2460c5ae4f2277fbb708e35536f3de3bf4
Zehra232006 <mehdizehra31@gmail.com>
2024-08-24 19:12:59 +0530

    Learn

--- code/2-component-data/start/src/App.jsx
+++ code/2-component-data/start/src/App.jsx
@@ -7,12 +7,25 @@ import RecipeTitle from './RecipeTitle'
 
 function App() {
     // TODO: Add recipe object
-
+    const recipe = {
+        title: 'Mashed potatoes',
+        feedback: {
+            rating: 4.8,
+            reviews: 20
+        },
+        ingredients: [
+            { name: '3 potatoes, cut into 1/2" pieces', prepared: false },
+            { name: '4 Tbsp butter', prepared: false },
+            { name: '1/8 cup heavy cream', prepared: false },
+            { name: 'Salt', prepared: true },
+            { name: 'Pepper', prepared: true },
+        ],
+    };
     return (
         <article>
             <h1>Recipe Manager</h1>
             {/* TODO: Add RecipeTitle component */}
-
+            <RecipeTitle title={ recipe.title } feedback={ recipe.feedback } />
             {/* TODO: Add IngredientList component */}
 
         </article>
--- code/2-component-data/start/src/RecipeTitle.jsx
+++ code/2-component-data/start/src/RecipeTitle.jsx
@@ -1,3 +1,4 @@
+import './RecipeTitle.css';
 import React from 'react';
 
 // TODO: Create RecipeTitle component
@@ -5,6 +6,9 @@ function RecipeTitle(props) {
     return (
         <section>
             <h2>{ props.title }</h2>
+            <h3 className={ props.feedback.rating <= 3.5 ? 'red' : 'green' }>
+                { props.feedback.rating } from { props.feedback.reviews } reviews
+            </h3>
         </section>
     )
 };
